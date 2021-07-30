e8087b9e41626f7599f165b1542dbdeeb193c3fc
unqownexe <76444282+unqownexe@users.noreply.github.com>
2021-07-30 12:36:52 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const config = require('../config.js');
 module.exports = async client => {
   client.user.setPresence({ activity: { type: "WATCHING", name: `Unqown Exe`}, status: 'idle' })
+  console.log("-Aktif-")
 };
 
 // Type kısımları:
